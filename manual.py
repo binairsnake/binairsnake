224903703dfb9da718d1896f26e32d044fdd569f
Herman <herman.alberts@gmail.com>
2024-09-01 21:52:38 +0200

    Add files via upload
    the read me file for the python place manager

---
+++ manual.py
@@ -0,0 +1,21 @@
+uitleg='''
+Read me manual explaining the detals of the tkinter place manager
+
+In the top row there is:
+Button "Dragable: Yes or No" This enables to lock / unlock the widgets in the right frame. Standard is "yes"
+Entry "Width" Which enables to adjust the width of the selected widget in the right frame.
+Entry "Height" Which enables to adjust the height of the selected widget in the right frame.
+Button "Apply" Which confirms the height and width.
+
+Button "Delete selected widget" Which deletes the selected widget in the right frame.
+Button "Save file as python-file"Which saves the configuration of the widgets in the right frame, 
+    This file can be used as GUI for your other application.
+Button "Read Me" Shows this readme manual.
+
+LeftFrame:
+In the left frame are examples of widgets. These widgets will be copied to the right frame when 
+the mouse is on top of the widget and clicked by the left mouse button. 
+
+Widgets available are: Labl, Button, Entry, Listbox, Checkbutton, Radiobutton, Scale, Text and Spinbox
+The names of the widgets will been showed on top of the left frame when hoovered by the mouse.
+'''
